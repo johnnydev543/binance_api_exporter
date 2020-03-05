bcfed5df1735a09f08ffffc50666763f58f6d04a
Jheng-lin (Johnny) Jiang <nick2nkite@hotmail.com>
2020-03-05 21:30:50 +0800

    re-write by another way

--- script.py
+++ script.py
@@ -7,54 +7,46 @@ API_KEY = "hQFQVuu78HvooYCQh0vvGs7RRkscDvCMaTnESBLXWZvcCDWGnLnkL6kNCAEsbeHS"
 API_SECRET = "9FS3yUCQD6Ancz8FqhTfbrPe65vbU2nqGA5wbE7laLJtHuS8U9y7q4grxW3bzs7c"
 client = Client(API_KEY, API_SECRET)
 
-# there is no lending endpoint api by far, so this is a tradeoff resort
+# lending endpoint is not implemented in the package,
+# so we need to force it to use the url and version we want
 client.API_URL = 'https://api.binance.com/sapi'
 client.PRIVATE_API_VERSION = "v1"
 
-
-class JenkinsCollector(object):
+class BinanceLendingsCollector(object):
 
     def collect(self):
-        metric_avgAnnualInterestRate = GaugeMetricFamily(
-            'binance_lending_avgAnnualInterestRate',
-            'Binance API lending avgerage annual interestRate',
-            labels=["asset"])
 
-        metric_purchasedAmount = GaugeMetricFamily(
-            'binance_lending_purchasedAmount',
-            'Binance API lending purchased amount',
-            labels=["asset"])
-
-        metric_upLimit = GaugeMetricFamily(
-            'binance_lending_upLimit',
-            'Binance API lending upLimit',
-            labels=["asset"])
-
-        params = {}  # {'status': 'ALL'}
-
+        params = {}
         lendings = client._get("lending/daily/product/list",
                                True, client.PUBLIC_API_VERSION, data=params)
-        for asset in lendings:
-            name = asset['asset']
 
-            # If there's a null result, we want to export a null.
-            avgAnnualInterestRate = asset['avgAnnualInterestRate'] or null
-            purchasedAmount = asset['purchasedAmount'] or null
-            upLimit = asset['upLimit'] or null
+        # The metrics we want to export.
+        statuses = ['avgAnnualInterestRate', 'purchasedAmount', 'upLimit']
 
-            metric_avgAnnualInterestRate.add_metric(
-                [name], avgAnnualInterestRate)
-            metric_purchasedAmount.add_metric([name], purchasedAmount)
-            metric_upLimit.add_metric([name], upLimit)
+        metrics = {}
 
-        yield metric_avgAnnualInterestRate
-        yield metric_purchasedAmount
-        yield metric_upLimit
+        # use "for loop" to create gauge list
+        for s in statuses:
+            metrics[s] = GaugeMetricFamily(
+                'binance_lending_{0}'.format(s),
+                'Binance API lendings product ' + s + ' data',
+                labels=["asset"])
+
+        # just get the value which only in the statuses list
+        for product in lendings:
+            asset_name = product.get('asset')
+
+            for key in product:
+                if key in statuses:
+                    metrics[key].add_metric([asset_name], product.get(key, 0))
+
+        for m in metrics.values():
+            yield m
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    REGISTRY.register(JenkinsCollector())
+    REGISTRY.register(BinanceLendingsCollector())
     start_http_server(5000)
     while True:
         time.sleep(10)
